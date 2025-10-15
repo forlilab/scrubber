ec5feac2fe2521a19c94f6e189e0dbb1138ca775
Joani <mato.joani@gmail.com>
2025-10-15 16:00:41 -0700

    Update setup.py -- version change

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def find_files(directory):
 setup(
     name="molscrub",
     author="Forli Lab",
-    version="0.1.1",
+    version="0.2.0",
     author_email="forli@scripps.edu",
     url="https://github.com/forlilab/molscrub",
     description="Enumerate states of small organic molecules: 3D coordinates, tautomers, pH-based adjustments",
