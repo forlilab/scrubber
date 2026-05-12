200de0d51fc32e6ec313964cd5dfbc0900352cdb
Joani Mato <mato.joani@gmail.com>
2026-05-12 12:55:56 -0700

    fix: loading model when not using the cli

--- molscrub/core.py
+++ molscrub/core.py
@@ -19,6 +19,64 @@ from .openff_toolkit import EspalomaMinimizer
 from .openff_toolkit import EspalomaCharger, NaglCharger
 from .geometry import gen3d
 
+import joblib
+import tempfile
+import urllib.request
+import shutil
+from pathlib import Path
+
+
+#handle pka model downloading. 
+MODEL_URL = "https://github.com/forlilab/pkaPrediction/raw/refs/heads/main/models/ETR_latest_compressed.joblib"
+MODEL_FILENAME = "ETR_latest_compressed.joblib"
+
+CACHE_DIR = Path.home() / ".cache" / "molscrub"
+MODEL_PATH = CACHE_DIR / MODEL_FILENAME
+
+
+def download_model():
+    """
+    Download pKa model and store in chace directory
+    `$HOME/.cache/molscrub`
+    """
+    CACHE_DIR.mkdir(parents=True, exist_ok=True)
+
+    print()
+    print("Downloading pickled ML model (one time only) from:")
+    print(MODEL_URL)
+    print("This will be saved in $HOME/.cache/molscrub/")
+    print()
+
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        urllib.request.urlretrieve(MODEL_URL, tmp_file.name)
+        tmp_path = Path(tmp_file.name)
+
+    shutil.move(str(tmp_path), MODEL_PATH)
+
+
+def verify_model():
+    """
+    Check if model is in cach directory. If not, download. 
+    """
+    if not MODEL_PATH.exists():
+        download_model()
+    else:
+        print()
+        print("pKa ML model found: ", MODEL_PATH)
+        print("download skipped")
+        print()
+
+
+def load_model():
+    """
+    Load and return etr1 model. Checks if it's available and 
+    downloads into .cache directory of it's not. 
+    """
+    verify_model()
+    model = joblib.load(MODEL_PATH)
+    return model
+
+
 class Scrub:
 
     def __init__(
@@ -48,6 +106,11 @@ class Scrub:
         charge_model=None,
         debug=False,
     ):
+        
+        # this is needed if using API instead of CLI
+        if pka_model != "rules" and model_file == None:
+            model_file = load_model()
+
         if pka_fname is None:
             self.acid_base_conjugator = AcidBaseConjugator.from_default_data_files(model=model_file)
         else:
--- scripts/scrub.py
+++ scripts/scrub.py
@@ -6,15 +6,13 @@ import json
 import multiprocessing
 from os import linesep
 import pathlib
-from pathlib import Path
+
 import sys
-import joblib
-import tempfile
-import urllib.request
-import shutil
+
 
 from molscrub import Scrub
 from molscrub import SMIMolSupplierWrapper
+from molscrub.core import load_model
 
 from rdkit import Chem
 from rdkit import RDLogger
@@ -26,53 +24,6 @@ Chem.SetDefaultPickleProperties(Chem.PropertyPickleOptions.MolProps |
 RDLogger.DisableLog("rdApp.*")
 
 console = Console()
-
-
-#handle pka model downloading. 
-MODEL_URL = "https://github.com/forlilab/pkaPrediction/raw/refs/heads/main/models/ETR_latest_compressed.joblib"
-MODEL_FILENAME = "ETR_latest_compressed.joblib"
-
-CACHE_DIR = Path.home() / ".cache" / "molscrub"
-MODEL_PATH = CACHE_DIR / MODEL_FILENAME
-
-
-def download_model():
-    """
-    Download pKa model and store in chace directory
-    `$HOME/.cache/molscrub`
-    """
-    CACHE_DIR.mkdir(parents=True, exist_ok=True)
-
-    print()
-    print("Downloading pickled ML model (one time only) from:")
-    print(MODEL_URL)
-    print("This will be saved in $HOME/.cache/molscrub/")
-    print()
-
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        urllib.request.urlretrieve(MODEL_URL, tmp_file.name)
-        tmp_path = Path(tmp_file.name)
-
-    shutil.move(str(tmp_path), MODEL_PATH)
-
-
-def verify_model():
-    """
-    Check if model is in cach directory. If not, download. 
-    """
-    if not MODEL_PATH.exists():
-        download_model()
-    else:
-        print()
-        print("pKa ML model found: ", MODEL_PATH)
-        print("download skipped")
-        print()
-
-
-def load_model():
-    verify_model()
-    model = joblib.load(MODEL_PATH)
-    return model
 
 try:
     import h5py
